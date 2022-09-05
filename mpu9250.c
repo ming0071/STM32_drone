60fbe20095767de716f76a200a1a1984832a73d2
ming0071 <112502009+ming0071@users.noreply.github.com>
2022-09-05 21:29:11 +0800

    Update mpu9250.c

--- mpu9250.c
+++ mpu9250.c
@@ -523,8 +523,8 @@ void GetMagBiasData()
 }
 void GetIMUData(float ASA[3])
 {
-#define print_flag 0 // 0-> no work , 1 ->printf acc, 2 ->printf gyro , 3 ->printf mag
-                     //             , 4 ->printf all data
+#define print_flag 0 // 0-> no printf , 1 ->printf acc, 2 ->printf gyro , 3 ->printf mag
+                     //               , 4 ->printf all data
 
     static short mag_count = 0;
     // Read ACCEL----------------------------------------------------------------------------------
